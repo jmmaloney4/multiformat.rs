530c44a8a8494f41bad596757e2f4e51b6b7d11f
Jack Maloney <git@jmmaloney4.xyz>
2021-10-21 22:03:41 -0500

    Add ntet_group_to_octets

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ pub mod multibase {
     pub struct Multibase {}
 
     use anyhow::{ensure, Result};
-    use num::integer::{div_ceil, lcm};
+    use num::integer::{div_ceil, div_floor, lcm};
 
     fn octet_group_to_ntets(input: Vec<u8>, n: u8) -> Result<Vec<u8>> {
         // N-tets can only be 1-7 in size
@@ -119,6 +119,74 @@ pub mod multibase {
         Ok(output)
     }
 
+    fn ntet_group_to_octets(input: Vec<u8>, n: u8) -> Result<Vec<u8>> {
+        // N-tets can only be 1-7 in size
+        ensure!(0 < n && n < 8, "Invalid N: {}", n);
+
+        // Handle trivial case
+        if input.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        // Number of n-tets it takes to make an even octet boundary.
+        let m = lcm(8, n) / n;
+
+        // Verify that we are not handling more than one "group" of n-tets at a time.
+        ensure!(
+            input.len() <= m.into(),
+            "Invalid input size: {} for given n: {}",
+            input.len(),
+            n
+        );
+
+        let out_len = div_floor(input.len() * n as usize, 8);
+
+        let mut ntets = input;
+        ntets.resize(m.into(), 0);
+
+        let mut output = vec![0; (lcm(8, n) / 8).into()];
+        // Index into output
+        let mut j: usize = 0;
+
+        let mut offset: u8 = 0;
+        let mut q: u8;
+        let mut r: u8 = 0;
+
+        for i in ntets.into_iter() {
+            ensure!(i < pow2(n), "Invalid {}-tet: {}", n, i);
+
+            // Handle carry. Take the least significant part of the n-tet (r) and
+            // move it to the most significant part of the next output byte.
+            if r != 0 {
+                output[j] += r * pow2(8 - offset);
+            }
+            // We have handled the remainder now.
+            r = 0;
+
+            offset += n;
+            if offset < 8 {
+                // We have not crossed a byte boundary, so the entire input n-tet is in the current output byte.
+                output[j] += i * pow2(8 - offset);
+            } else {
+                // We have crossed a byte boundary, we need to split the most and least significant halves of the input n-tet
+                q = i / pow2(offset - 8);
+                r = i % pow2(offset - 8);
+                output[j] += q;
+                j += 1;
+            }
+
+            offset = offset % 8
+        }
+        
+        let x = output.split_off(out_len);
+        println!("{:#?}", x);
+
+        ensure!(r == 0 && x.iter().all(|i| -> bool { *i == 0 as u8 }), "Not Canonical N-Tet");
+
+        // Already appropriately truncated by .split_off above
+        Ok(output)
+    }
+
     fn pow2(i: u8) -> u8 {
         2_u8.pow(i.into())
     }
@@ -138,6 +206,21 @@ pub mod multibase {
             );
 
             assert_eq!(super::octet_group_to_ntets(vec![77], 6).unwrap(), [19, 16]);
+        }
+
+        #[test]
+        fn test_ntet_group_to_octets() {
+            assert_eq!(
+                super::ntet_group_to_octets(vec![19, 22, 5, 46], 6).unwrap(),
+                [77, 97, 110]
+            );
+
+            assert_eq!(
+                super::ntet_group_to_octets(vec![19, 22, 4], 6).unwrap(),
+                [77, 97]
+            );
+
+            assert_eq!(super::ntet_group_to_octets(vec![19, 16], 6).unwrap(), [77]);
         }
     }
 }
